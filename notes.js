ae5b679e22c4e7fd4c077765be687ffb2197848c
Belchenkov <u608110@gmail.com>
2017-10-15 19:08:06 +0300

    Catch Paramets

---
+++ notes.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const comment = process.argv[2];
+const title = process.argv[3];
+const content = process.argv[4];
+
+switch (command) {
+    case 'list':
+        break;
+    case 'view':
+        break;
+    case 'create':
+        break;
+    case 'remove':
+        break;
+    default:
+        console.log('Uknown Command');
+}
