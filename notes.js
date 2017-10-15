2f88bc31f68851281158028aad46f17f85f813ec
Belchenkov <u608110@gmail.com>
2017-10-15 19:23:00 +0300

    Create Command list

--- notes.js
+++ notes.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 
-const comment = process.argv[2];
+const command = process.argv[2];
 const title = process.argv[3];
 const content = process.argv[4];
 
-switch (command) {
+switch(command) {
     case 'list':
+        list((error, notes) => {
+            if (error) return console.error(error.message);
+
+            notes.forEach((note, index) => console.log(`${index + 1}. ${note.title}`));
+        }); 
         break;
     case 'view':
+        view();
         break;
     case 'create':
+        create();
         break;
     case 'remove':
+        remove();
         break;
     default:
         console.log('Uknown Command');
+}
+
+function list(done) {
+    fs.readFile('notes.json', (error, data) => {
+        if (error) return done(error);
+        
+        const notes = JSON.parse(data);
+        done(null, notes);
+    });
 }
